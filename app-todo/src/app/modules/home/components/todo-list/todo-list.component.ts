bd320decfa57fca2542e01728c45fcc2f6b598f3
marjoriegaioso <marjoriegaioso99@gmail.com>
2023-01-04 09:56:06 -0300

    projeto concluido

--- app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 //Interface
 import { TaskList } from '../../model/task-list';
 
@@ -7,15 +7,43 @@ import { TaskList } from '../../model/task-list';
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent implements OnInit {
+export class TodoListComponent implements DoCheck {
 
-  public taskList: Array<TaskList> = [
-    {task: "Minha nova tarefa", checked: true},
-    {task: "Minha nova tarefa 2", checked: false}
-  ];
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem("list") || '[}');
 
   constructor(){ }
+  ngDoCheck(): void {
+    this.setLocalStorage();
+    }
 
-  ngOnInit(): void {
+  public setEmitTaskList (event: string){
+    this.taskList.push({task: event, checked: false});
+  }
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Você deseja apagar todas as suas tarefas?");
+    if (confirm) {
+    this.taskList = [];
+  }
+ }
+
+ public validationInput(event: string, index: number){
+
+  if (!event.length){
+    const confirm = window.confirm("Task está vazia, deseja deletar?");
+
+    if(confirm){
+      this.deleteItemTaskList(index);
+    }
+  }
+ }
+  public setLocalStorage(){
+    if (this.taskList){
+      this.taskList.sort( (first, last) => Number(first.checked) - Number (last.checked) )
+      localStorage.setItem("list", JSON.stringify(this.taskList))
+    }
   }
 }
